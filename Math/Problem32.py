3a30575511f06b7a162a51fbebdc6599ebeae683
Ayesha A <ayesha.s.abduljalil@gmail.com>
2022-10-19 10:40:15 +0300

    Add files via upload

---
+++ Math/Problem32.py
@@ -0,0 +1,12 @@
+'''
+Read four numbers, a, b, c, and d, and print the result of a ^ b + c ^ d.
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__=="__main__":
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    d=int(input())
+    
+    print((a**b)+(c**d))
