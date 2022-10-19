64fc5a3d781a2a186badb1c3215936a5f3120fe9
Ayesha A <ayesha.s.abduljalil@gmail.com>
2022-10-19 10:42:20 +0300

    Add files via upload

---
+++ Math/Problem33.py
@@ -0,0 +1,9 @@
+'''
+You are given a positive integer N. Print a numerical triangle of height N-1.
+Can you do it using only arithmetic operations, a single for loop and print statement?
+
+Use no more than two lines. The first line (the for statement) is already written for you. You have to complete the print statement.
+'''
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(i * (((10 ** i) - 1) // 9))
